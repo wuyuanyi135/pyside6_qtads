14ba084d81aa08680513b1a657d6af1e98ce2b91
Matt Borgerson <contact@mborgerson.com>
2022-11-10 17:23:38 -0700

    Fix ADS_VERSION

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setuptools.setup(
             cmake_configure_options=[
                 "-DBUILD_EXAMPLES:BOOL=OFF",
                 "-DBUILD_STATIC:BOOL=ON",
-                "-DADS_VERSION=1.2.3", # Dummy version
+                "-DADS_VERSION=3.8.3",
                 f"-DPython3_ROOT_DIR={Path(sys.prefix)}"
             ],
             py_limited_api=True
